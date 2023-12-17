1a5f2b8df486146ea73fe9060f788a69e0f81e12
Janek Lauer <git@lauer.email>
2023-12-17 15:06:28 +0100

    update

--- common.go
+++ common.go
@@ -1,8 +1,25 @@
 package main
 
+import (
+	"math/rand"
+)
+
+type linkType int
+
+const (
+	redirectLink linkType = 0
+	textLink     linkType = 1
+	validRunes   string   = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789"
+)
+
 type link struct {
-	id    string
-	value string
+	id       string
+	value    string `form:"value"`
+	linkType int    `form:"linkType"`
+}
+
+type config struct {
+	baseUrl string `yaml:"baseUrl"`
 }
 
 func trim(text string, maxLength int) string {
@@ -11,4 +28,12 @@ func trim(text string, maxLength int) string {
 		return text
 	}
 	return string(runes[0:maxLength])
+}
+
+func randomString(length int) string {
+	stringBytes := make([]byte, length)
+	for i := range stringBytes {
+		stringBytes[i] = validRunes[rand.Intn(len(validRunes))]
+	}
+	return string(stringBytes)
 }
